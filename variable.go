880aa2d646c71ece992b17cc1e0e69e8a6d11630
Shivkumar13 <shivkumar.ople97@gmail.com>
2019-05-19 18:14:49 +0530

    varible scope VAR

---
+++ variable.go
@@ -0,0 +1,29 @@
+
+
+
+package main
+
+import "fmt"
+
+
+func bar() {
+	fmt.Println(x)
+}
+
+
+var x=10
+
+func main() {
+
+	y:= 20
+	fmt.Println(y)
+	z := 430 + 567
+	fmt.Println(z)
+	bar()	
+	foo()
+}
+
+func foo() {
+
+	fmt.Println(x)
+}

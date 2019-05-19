844590ac1cbee2d5b205e5c9de314ace8e6c1540
Shivkumar13 <shivkumar.ople97@gmail.com>
2019-05-19 17:21:32 +0530

    operator.go

---
+++ short_declaration_operator.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt" 
+ func main() {
+      	x := 42
+	fmt.Println(x)
+	x = 23
+	fmt.Println(x)
+	y := 45 + 56
+	fmt.Println(y)
+
+	z := ("bond, james")
+	fmt.Println(z)
+	
+
+ }

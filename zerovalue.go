2c3753fa676b01c1805a99be31439b1e090c6a0d
Shivkumar13 <shivkumar.ople97@gmail.com>
2019-05-22 01:31:07 +0530

    zero value

---
+++ zerovalue.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var y int
+
+var z string
+
+func main() {
+
+	fmt.Println("Containers are Linux", z , "Linux is Containers")
+	fmt.Printf("%T\n", z)
+
+	fmt.Println("4",y ,"8")
+	fmt.Printf("%T", y)
+
+}

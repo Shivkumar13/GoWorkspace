605885bca8211154213cfb3c5218fa5c4adbd38f
Shivkumar13 <shivkumar.ople97@gmail.com>
2019-05-25 13:02:20 +0530

    various Go format specifiers

---
+++ fmt.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+var y = 22 
+
+// print TYPE
+fmt.Printf("%T\n", y) 
+
+//print BINARY  format
+fmt.Printf("%b\n", y)
+//print base 16
+fmt.Printf("%x\n", y)
+fmt.Printf("%b\n", y)
+fmt.Printf("%x\n", y)
+y = 456
+fmt.Printf("%#x\t%b\t%x", y, y, y)
+
+}

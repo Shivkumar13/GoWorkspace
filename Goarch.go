62caaff55e644d4238b81759dd655c4bb92cc720
Shivkumar Ople <sople@redhat.com>
2020-11-26 17:44:37 +0530

    Adding Goarch.go

--- Goarch.go
+++ Goarch.go
@@ -5,18 +5,14 @@ import (
 	"runtime"
 )
 
-
-
 func main() {
 
 	//Printing some runtime values with runtime package.
-
 
 	fmt.Println(runtime.GOMAXPROCS)
 	fmt.Println(runtime.GOARCH)
 	fmt.Println(runtime.GoroutineProfile)
 	fmt.Println(runtime.GOROOT)
 	fmt.Println(runtime.GOOS)
-
 
 }

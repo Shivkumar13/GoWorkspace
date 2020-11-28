0a27fb68310dcd494606d0b86a028836c30ec70e
Shivkumar Ople <sople@redhat.com>
2020-11-28 21:17:44 +0530

    Adding ninja2-operators.go

---
+++ operators.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	a := (56 == 56)
+	b := (56 <= 57)
+	c := (58 >= 01)
+	d := (67 != 56)
+	t := (89 < 90)
+	r := (89 > 23)
+
+	fmt.Println(a, b, c, d, t, r)
+
+}

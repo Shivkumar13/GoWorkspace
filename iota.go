4439f302c61c2ab247c1f39583cabbc7281099c9
Shivkumar Ople <sople@redhat.com>
2020-11-28 19:54:31 +0530

    Adding iota.go

---
+++ iota.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+const (
+	a = iota
+	z
+	h
+	t
+)
+
+const (
+	y = iota
+	o
+	plll
+)
+
+func main() {
+
+	const ople = iota
+
+	fmt.Println(a)
+	fmt.Println(z)
+	fmt.Println(h)
+	fmt.Println(t)
+	fmt.Println(y)
+	fmt.Println(o)
+	fmt.Println(plll)
+	fmt.Println(ople)
+
+}

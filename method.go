d454e6d1865b3db8074e35fe10a4db92b2156d3d
Shivkumar13 <shivkumar.ople97@gmail.com>
2021-05-28 23:21:58 +0530

    Adding Method.go

--- method.go
+++ method.go
@@ -14,30 +14,42 @@ type secretAgent struct {
 	ltk bool
 }
 
-// func (r receiver) identifier(parameters) (return(s)) { code }
-
+// func (r receiver) identifier(parameters) (return(s)) { Code }
 func (s secretAgent) speak() {
+
 	fmt.Println("I am", s.first, s.last)
 }
 
 func main() {
+
 	sa1 := secretAgent{
 		person: person{
-			"James",
-			"Bond",
+			first: "James",
+			last:  "Bond",
 		},
+
 		ltk: true,
 	}
 
 	sa2 := secretAgent{
+
 		person: person{
-			"Miss",
-			"Moneypenny",
+			first: "Shivkumar",
+			last:  "Ople",
 		},
-		ltk: true,
+
+		ltk: false,
 	}
 
-	fmt.Println(sa1)
+	fmt.Println("Hello, playground")
+	x := sa1.first
+	y := sa2.ltk
+
+	fmt.Println(x)
+	fmt.Println(y)
+
+	fmt.Println("----")
+
 	sa1.speak()
 	sa2.speak()
 }

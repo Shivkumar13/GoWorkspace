6b854ae1f9d972726b92f5be3da5d949112b1963
Shivkumar13 <shivkumar.ople97@gmail.com>
2021-07-31 23:47:33 +0530

    Grouping Data with Slice

---
+++ slice-new.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	//Composite Literal
+
+	x := []int{4, 5, 6, 7, 8}
+	fmt.Println(x)
+
+}
+
+//SLICE allows you to group together VALUES of the SAME TYPE.

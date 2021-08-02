bef54117ebf5fb4fa8909232ff3b356498fbdc8b
Shivkumar13 <shivkumar.ople97@gmail.com>
2021-08-03 01:24:28 +0530

    Adding deleting-from-a-slice.go

---
+++ append_to_a_slice1.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	x := [...]int{5, 6, 7, 8, 89, 90}
+
+	//Cut the Slice so that it should look like {6,7,8}
+	fmt.Println(x[1:4])
+
+	//Cut the Slice such that it should look like {89,90}
+	fmt.Println(x[4:])
+
+	s := x[0:2]
+	g := x[0:4]
+	k := x[4:5]
+	l := x[:2]
+	//i := string(g) + string(k)
+	fmt.Println(s, g, k)
+
+	fmt.Println("This is x[:2]", l)
+
+	//cut the slice such that it should look like {5,6,89}
+
+	i := append(s, k...)
+	fmt.Println(i)
+
+}

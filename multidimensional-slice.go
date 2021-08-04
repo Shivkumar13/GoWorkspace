bb2cafbf1cb3b74633922590ee2fc68effc450c2
Shivkumar13 <shivkumar.ople97@gmail.com>
2021-08-05 04:27:27 +0530

    multi-dimensional slice

---
+++ multidimensional-slice.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	jp := []string{"Shivkumar", "sddfd", "sdsdfd", "asdsf"}
+
+	gp := []string{"sdfgfgdg", "dsdfgfd", "sdfdfgg", "sdfdghdh"}
+
+	mp := [][]string{jp, gp}
+
+	fmt.Println(mp)
+}

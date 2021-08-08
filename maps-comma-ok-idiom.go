87abbb9a2aff4dccee319e5c52fd542d36a9d6ad
Shivkumar13 <shivkumar.ople97@gmail.com>
2021-08-09 01:26:32 +0530

    Maps with ok-idiom

---
+++ maps-comma-ok-idiom.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	m := map[string]int{
+		"a": 65,
+		"b": 66,
+	}
+
+	g := map[int]int{
+		12: 1,
+		34: 6,
+	}
+	fmt.Println(m)
+	fmt.Println(m["a"])
+	fmt.Printf("m is a %T\n", m)
+	fmt.Printf("m is a %v\n", m)
+	fmt.Println("Starting g here")
+	fmt.Println(g)
+	fmt.Println(g[12])
+	fmt.Printf("g is a %T\n", g)
+
+	fmt.Println("Starting , ok idiom here")
+
+	v, d := g[12] //", ok" Idiom
+
+	fmt.Println(v)
+	fmt.Println(d)
+
+	// ", ok" Idiom  with IF
+	fmt.Println("Starting , ok idiom with IF here")
+
+	if h, ok := g[34]; ok {
+
+		fmt.Println(h)
+		fmt.Println(ok)
+	}
+}
+
+//		fmt.Println("The value is present", )

171d97f4b0f1a68d6414cce6e95c930117eb65c5
Shivkumar13 <shivkumar.ople97@gmail.com>
2021-08-12 00:32:55 +0530

    Adding ninja 3-array,slice-with-cut-slice

--- ninja3-slice.go
+++ ninja3-slice.go
@@ -6,10 +6,15 @@ func main() {
 
 	s := [10]int{2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
 
+	s1 := s[0:4]
+	s2 := s[4:8]
+	s3 := s[8:10]
+
 	for k, v := range s {
 
 		fmt.Printf("The values of slice s is %d for %d\n", v, k)
 	}
 
 	fmt.Printf("%T\n", s)
+	fmt.Println(s1, s2, s3)
 }

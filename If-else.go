2d439e0739b2f79291d5ab8d50f4d4e2054b0ebd
Shivkumar13 <shivkumar.ople97@gmail.com>
2021-11-18 02:50:30 +0530

    fallthrough and conditionals

---
+++ If-else.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	if 8 >= 0 {
+		fmt.Println("True")
+
+	}
+
+	if a := 1; a < 0 {
+
+		fmt.Println("a is positive")
+
+	} else {
+		fmt.Println("a is positive")
+	}
+}
